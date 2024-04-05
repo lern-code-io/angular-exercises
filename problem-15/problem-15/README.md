7ca2245ea8c2ce51d78cf6e31ab0e53528cc19af
LukeBraverman <l.braverman.l@live.co.uk>
2024-04-05 20:52:36 +0100

    Problem 15

---
+++ problem-15/problem-15/README.md
@@ -0,0 +1,13 @@
+# Task 15: Reactive form validation (lv2)
+
+## Description
+In the 'ReactiveFormComponent', two validators to the name form control. The validators should ensure that the 
+name input is not blank, and is more than 4 characters.
+
+## Acceptance Criteria
+- Add two validators to the name form control to ensure input is not blank and more than 4 characters
+- Load up the application. Submit with name input empty and with a name with less than 4 characters - an invalid message 
+  should show.
+
+## Useful Links
+- [Reactive Forms Validation](https://angular.dev/guide/forms/form-validation#)
---
+++ problem-15/problem-15/src/app/components/reactive-form/reactive-form.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+
+@Component({
+  imports: [ReactiveFormsModule, NgIf],
+  selector: 'app-reactive-form',
+  standalone: true,
+  styleUrl: './reactive-form.component.scss',
+  template: `
+    <form *ngIf="nameForm" [formGroup]="nameForm"  (ngSubmit)="onSubmit()" class="main-container">
+      <label for="name">Name: </label>
+      <input id="name" type="text" formControlName="name">
+      <button type="submit">Submit!</button>
+      <p *ngIf="formIsInvalid"> This form is Invalid!</p>
+      <p *ngIf="validSubmission"> This form is Valid!</p>
+    </form>
+  `
+})
+export class ReactiveFormComponent implements OnInit {
+
+
+  nameForm: FormGroup | undefined = undefined;
+  formIsInvalid: boolean = false;
+  validSubmission: boolean = false;
+
+  ngOnInit(): void {
+    this.nameForm = new FormGroup({
+      name: new FormControl('',
+          [
+              // insert validators inside here
+            Validators.required,
+            Validators.minLength(4),
+          ])
+    })
+  }
+
+  onSubmit() {
+    console.log('called')
+    if (!this.nameForm?.valid)
+    {
+      this.formIsInvalid = true;
+      this.validSubmission = false;
+    } else {
+      this.formIsInvalid = false;
+      this.validSubmission = true;
+    }
+  }
+}
