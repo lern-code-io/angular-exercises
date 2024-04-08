d1db41accfb07a9f17855b822b9511b0de4c3435
LukeBraverman <l.braverman.l@live.co.uk>
2024-04-08 10:12:46 +0100

    Removing 21-40

--- problem-27/src/app/guards/can-deactivate.guard.ts
+++
@@ -1,6 +0,0 @@
-import {CanDeactivateFn} from '@angular/router';
-
-export const canDeactivateGuard: CanDeactivateFn<any> = (route, state) => {
-  // TODO: Prevent the user from navigating away from admin page
-  return false;
-};
